acbd372e3c1d95c36c40c3d25bf338d9ea462b82
Fégan <fegan.pierre@gmail.com>
2021-06-18 17:26:13 +0200

    Init Strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0103e736f38ed794d4866dd5cfa9e9ea'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2c4b731-eb32-4e73-ad42-e6e46b921471'
+};
